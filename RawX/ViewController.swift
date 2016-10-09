3092b976b76bd4d41efa077cf43f1ed86ed42b08
Me <kevinrogers@kevins-macbook.local>
2016-10-08 20:24:32 -0400

    Initial Commit

---
+++ RawX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RawX
+//
+//  Created by Kevin Rogers on 10/8/16.
+//  Copyright © 2016 Kevin Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
